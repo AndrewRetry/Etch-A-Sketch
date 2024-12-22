c225cea8fd222ae768e056063977250a5e775e51
AndrewRetry <[andrew.retryans@gmail.com]>
2024-12-22 17:57:47 +0800

    painting enabled if mousedown is triggered inside palette

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
-let squaresPerSide = 16 //default value
-let squareQuantitySlider = document.querySelector("#squareQuantity")
+let squaresPerSide = 16; //default value
+let squareQuantitySlider = document.querySelector("#squareQuantity");
 squareQuantitySlider.addEventListener("change", ()=> {
-    squaresPerSide = squareQuantitySlider.value
+    squaresPerSide = squareQuantitySlider.value;
     createSquares();
 });
 
 const PALETTESIZE = 800;
-
 const paletteArea = document.querySelector('#palette');
 paletteArea.style.width = `${PALETTESIZE}px`;
 paletteArea.style.height = `${PALETTESIZE}px`;
 
+// check whether drawing mode or not
+let isDrawing = false;
+
+paletteArea.addEventListener("mousedown", (event) => {
+    isDrawing = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
+
+// event delegation for coloring squares
+paletteArea.addEventListener("mouseover", (event) => {
+    if (isDrawing && event.target.classList.contains("squares")) {
+        event.target.style.backgroundColor = colorPicked;
+    }
+});
 
 // changes background color of squares, trigerred w/ event handler
 let colorInput = document.querySelector("#colorPicker");
+let colorPicked = "black"
 colorInput.addEventListener("change", () => {
     colorPicked = colorInput.value;
 });
 
 function setBackgroundColor() {
     this.style.backgroundColor = colorPicked
-}
+};
 
-// event delegation for coloring squares
-paletteArea.addEventListener("mouseover", (event) => {
-    if (event.target.classList.contains("squares")) {
-        event.target.style.backgroundColor = colorPicked;
-    }
-})
 
 // Substract 2 to ensure no overflow
 function createSquares() {
@@ -39,7 +50,7 @@ function createSquares() {
         gridSquare.style.height = `${Math.floor(PALETTESIZE / squaresPerSide) -2}px`;
         gridSquare.classList.add("squares");
         paletteArea.appendChild(gridSquare);
-    }
-}
+    };
+};
 
-createSquares()
+createSquares();
