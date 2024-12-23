50fd091ef0d63d962cd1b46aa2cea580248fb397
AndrewRetry <[andrew.retryans@gmail.com]>
2024-12-23 14:25:18 +0800

    Changed toggle draw key, add isDrawingIndicator for clarity, add actionText that tracks current action for clarity

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-let squaresPerSide = 16; //default value
 let squareQuantitySlider = document.querySelector("#slider");
+let sliderValueString = document.querySelector("#sliderValue");
+let squaresPerSide = squareQuantitySlider.value; //default value
+
+sliderValueString.textContent = `Resolution : ${squaresPerSide} x ${squaresPerSide}`
+
 squareQuantitySlider.addEventListener("change", ()=> {
     squaresPerSide = squareQuantitySlider.value;
     // slider display value
-    let sliderValueString = document.querySelector("#sliderValue")
-    sliderValueString.textContent = `Resolution : ${squaresPerSide} x ${squaresPerSide}`
+    sliderValueString.textContent = `Resolution : ${squaresPerSide} x ${squaresPerSide}`;
     createSquares();
 });
 
@@ -15,31 +18,37 @@ paletteArea.style.height = `${PALETTESIZE}px`;
 
 // check whether drawing mode or not
 let isDrawing = false;
-
-paletteArea.addEventListener("mousedown", (event) => {
-    isDrawing = true;
-});
-
-document.addEventListener("mouseup", () => {
-    isDrawing = false;
+let isDrawingIndicator = document.querySelector("#isDrawingIndicator")
+isDrawingIndicator.innerHTML = "Press the <strong>R</strong> key to draw!"
+document.addEventListener("keyup", (event) => {
+    if (event.key === "R" || event.key === "r") {
+        if (!isDrawing) {
+            isDrawing = true; 
+            isDrawingIndicator.innerHTML = "You can draw now!";
+        } else {
+            isDrawing = false;
+            isDrawingIndicator.innerHTML = "Press the <strong>R</strong> key to draw!";
+        };
+    };
 });
 
 // assign square to another background color
 function colorSquare(target) {
     if (target.classList.contains("squares")) {
         target.style.backgroundColor = colorPicked
-    }
-}
+    };
+};
+
 // event delegation for coloring squares
 paletteArea.addEventListener("mouseover", (event) => {
     if (isDrawing && event.target.classList.contains("squares")) {
-        colorSquare(event.target)
-    }
+        colorSquare(event.target);
+    };
 });
 
 // changes selected color according to colorPicker, trigerred w/ event handler
 let colorInput = document.querySelector("#colorPicker");
-let colorPicked = "black"
+let colorPicked = "black";
 colorInput.addEventListener("change", () => {
     colorPicked = colorInput.value;
 });
@@ -56,14 +65,13 @@ function createSquares() {
         paletteArea.appendChild(gridSquare);
     };
 };
-
 createSquares();
 
 // erases whole palette
 function resetPalette() {
-    const squares = document.querySelectorAll(".squares") // [square-1, square-2, ...square-n]
+    const squares = document.querySelectorAll(".squares"); // [square-1, square-2, ...square-n]
     squares.forEach(square => {
-        square.style.backgroundColor = "white"
+        square.style.backgroundColor = "white";
     });
 }
 
@@ -71,45 +79,48 @@ function resetPalette() {
 const resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener("click", resetPalette);
 
+let actionText = document.querySelector(".randomColorContainer p")
 // event handler for eraserBtn
 const eraserBtn = document.querySelector("#eraserBtn")
 eraserBtn.addEventListener("click", () =>{
-    colorPicked = "#ffffff"
-    colorInput.value = colorPicked
+    actionText.textContent = "Erasing...";
+    colorPicked = "#ffffff";
+    colorInput.value = colorPicked;
 });
 
 //event handler for randomizeBtn
-const randomColorBtn = document.querySelector("#randomBtn")
-const colorDisplayRandom = document.querySelector(".bubble")
+const randomColorBtn = document.querySelector("#randomBtn");
+const colorDisplayRandom = document.querySelector(".bubble");
 const randomizeDigits = (min, max) => (min + Math.floor(Math.random() * (max - min + 1)));
 
 //initialize random rgb values
-let isRandomized = false
-randomColorBtn.addEventListener("click", ()=>{
+let isRandomized = false;
+const buttonContainer = document.querySelector(".flex-container");
+
+randomColorBtn.addEventListener("click", (event)=>{
     if (isRandomized === false) {
-        isRandomized = true
-        randomColorBtn.style.backgroundColor = "#0056b3"
-        eraserBtn.disable = true
-        colorInput.disable = true
+        isRandomized = true;
+        actionText.textContent = "Random Colors";
+        randomColorBtn.style.backgroundColor = "#0056b3";
+        eraserBtn.style.backgroundColor = containerColor;
     } else {
-        isRandomized = false
-        randomColorBtn.style.backgroundColor = "gray"
-        eraserBtn.disable = false
-        colorInput.disable = false
-    }
+        isRandomized = false;
+        actionText.textContent = "";
+        randomColorBtn.style.backgroundColor = "gray";
+    };
 
     function randomizeRGB() {
-        colorR = randomizeDigits(0,255)
-        colorG = randomizeDigits(0,255)
-        colorB = randomizeDigits(0,255)
-        valRGB = `rgb(${colorR}, ${colorG}, ${colorB})`
-        return valRGB
+        colorR = randomizeDigits(0,255);
+        colorG = randomizeDigits(0,255);
+        colorB = randomizeDigits(0,255);
+        valRGB = `rgb(${colorR}, ${colorG}, ${colorB})`;
+        return valRGB;
     }
     paletteArea.addEventListener("mouseover", (event) => {
         if (isDrawing && isRandomized && event.target.classList.contains("squares")) {
-            colorPicked = randomizeRGB()
-            colorSquare(event.target)
-            colorDisplayRandom.style.backgroundColor = colorPicked
-        }
+            colorPicked = randomizeRGB();
+            colorSquare(event.target);
+            colorDisplayRandom.style.backgroundColor = colorPicked;
+        };
     });
 });
